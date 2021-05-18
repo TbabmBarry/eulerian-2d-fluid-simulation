6179e662ca0fab50c6965ee362c311dbbaa1d63a
lbllbl93 <lbllbl93@gmail.com>
2021-05-18 14:35:11 +0200

    clothes

--- src/ParticleToy.cpp
+++ src/ParticleToy.cpp
@@ -220,11 +220,69 @@ static System* Hair() {
                                                       Vec2f(0.0f,0.0f) + Vec2f(-radius, 0.f),//暂时定义为(0,0)+(-r,0)
                                                       radius));
     }
+    // Add gravity and drag to all particles
+    sys->addForce(new GravityForce(sys->particles, Vec2f(0, -9.81f)));
+    return sys;
+}
 
+static System* Cloth() {
+    System* sys = new System(new EulerSolver(EulerSolver::SEMI));
 
+    const int xSize = 3, ySize = 3;
+    const float deltax = 2.0f/xSize, deltay = 2.5f/ySize;
+    int index = 0;
+    // Initialize particles
+    for (int j = 0; j < ySize; j++) {
+        for (int i = 0; i < xSize; i++) {
+            sys->addParticle(new Particle(Vec2f(-0.5f + i * deltax, 0.5f - j * deltay), 0.2f, index));
+            index++;
+        }
+    }
     // Add gravity and drag to all particles
     sys->addForce(new GravityForce(sys->particles, Vec2f(0, -9.81f)));
 
+    float ks = 50.0f;
+    float kd = 1.0f;
+
+    for (int j = 0; j < ySize; j++) {//up down
+        for (int i = 0; i < xSize - 1; i++) {
+            sys->addForce(new SpringForce(sys->particles[i + j * xSize],
+                                          sys->particles[i + 1 + j * xSize],
+                                          deltax, ks, kd));
+        }
+    }
+
+    for (int j = 0; j < ySize - 1; j++) {//right,left
+        for (int i = 0; i < xSize; i++) {
+            sys->addForce(new SpringForce(sys->particles[i + j * xSize],
+                                          sys->particles[i + (j + 1) * xSize],
+                                          sqrt(pow(deltay, 2) + pow(deltay, 2)), ks, kd));
+        }
+    }
+
+    for (int j = 0; j < ySize - 1; j++) {//diagonal
+        for (int i = 0; i < xSize - 1; i++) {
+            sys->addForce(new SpringForce(sys->particles[i + j * xSize],
+                                          sys->particles[i + 1 + (j + 1) * xSize],
+                                          sqrt(pow(deltax, 2) + pow(deltay, 2) + pow(deltay, 2)), ks, kd));
+        }
+    }
+
+    for (int y = 0; y < ySize - 1; y++) {//diagonal
+        for (int x = 1; x < xSize; x++) {
+            sys->addForce(new SpringForce(sys->particles[x + y * xSize],
+                                          sys->particles[x - 1 + (y + 1) * xSize],
+                                          sqrt(pow(deltax, 2) + pow(deltay, 2) + pow(deltay, 2)), ks, kd));
+        }
+    }
+
+    float radius = 0.05f;
+    sys->addConstraint(new CircularWireConstraint(sys->particles[0],
+                                                  Vec2f(0.0f,0.0f) + Vec2f(-radius, 0.f),
+                                                  radius));
+    sys->addConstraint(new CircularWireConstraint(sys->particles[xSize-1],
+                                                  Vec2f(0.0f,0.0f) + Vec2f(-radius, 0.f),
+                                                  radius));
     return sys;
 }
 
